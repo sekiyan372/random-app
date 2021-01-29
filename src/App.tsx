b7388764e8fb4c57c1f62fa499c89773439d8bf0
sekiyan372 <b1917181@planet.kanazawa-it.ac.jp>
2021-01-29 14:46:37 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import styled from 'styled-components'
 
-function App() {
+const App: React.FC = () => {
+  const [formView, setFormView] = useState<string>('')
+  const [obje, setObje] = useState<string>('')
+
+  const changeView = (e: any) => {
+    setFormView(e.target.value)
+  }
+
+  const returnRondom = (text: string) => {
+    const inputArray: string[] = text.split(' ')
+    setObje(inputArray[Math.floor(Math.random() * inputArray.length)])
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <All>
+      <h1>ランダムに何かを決めるサイト</h1>
+      <div className="input-area">
+        <label>入力</label>
+        <input type="text" value={formView} onChange={(e) => changeView(e)} />
+        <input type="submit" value="送信" onClick={() => returnRondom(formView)} />
+      </div>
+      <div className="result-area">
+        <p>{ obje }</p>
+      </div>
+    </All>
   );
 }
+
+const All = styled.div`
+  text-align: center;
+`
 
 export default App;
